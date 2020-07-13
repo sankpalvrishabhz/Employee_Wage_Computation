785eef40eefc1a047aefb0fbbb9ce96d0402d61c
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-14 00:21:45 +0530

    Function to get Work Hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,21 +6,25 @@ workingDays=20
 MaxHrsInMonth=100
 totalHours=0
 totalWorkingDays=0
-while [[ $totalHours -lt $MaxHrsInMonth || $totalWorkingDays -lt $workingDays ]]
-do
-	((totalWorkingDays++))
-	var=$((RANDOM%3))
-	case "$var" in
+function getWorkingHours()
+{
+	case $1 in
 		$isFullTime)
-        		dayHours=8
-	        	;;
+			dayHours=8
+			;;
 		$isPartTime)
-        		dayHours=4
-		        ;;
+			dayHours=4
+			;;
 		*)
 			dayHours=0
 			;;
 	esac
+	echo $dayHours
+}
+while [[ $totalHours -lt $MaxHrsInMonth && $totalWorkingDays -lt $workingDays ]]
+do
+	((totalWorkingDays++))
+	dayHours="$( getWorkingHours $((RANDOM%3)) )"
 	totalHours=$(($totalHours+$dayHours))
 done
 totalWages=$(($totalHours*$wagePerHr))
