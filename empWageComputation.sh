47f6ceb20f02f6d85ee9d56199fa43d7455a8465
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-14 00:11:37 +0530

    Wage of Month Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,16 +2,22 @@
 wagePerHr=20
 isFullTime=2
 isPartTime=1
-var=$((RANDOM%3))
-case "$var" in
-	$isFullTime)
-        	dayHours=8
-        	;;
-	$isPartTime)
-        	dayHours=4
-	        ;;
-	*)
-		dayHours=0
-		;;
-esac
-echo "Daily Wage=" $(($dayHours*$wagePerHr))
+workingDays=20
+for(( day=1; day<=$workingDays; day++ ))
+do
+	var=$((RANDOM%3))
+	case "$var" in
+		$isFullTime)
+        		dayHours=8
+	        	;;
+		$isPartTime)
+        		dayHours=4
+		        ;;
+		*)
+			dayHours=0
+			;;
+	esac
+	dailyWage=$(($dayHours*$wagePerHr))
+        totalWage=$(($totalWage+$dailyWage))
+done
+echo "Total Wage = " $totalWage
